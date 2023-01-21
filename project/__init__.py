b7a4893e3328d504d439f64da5564789d669d726
Anton Shefer <rezajkee@mail.ru>
2023-01-21 22:38:21 +0500

    Added advanced logging

--- project/__init__.py
+++ project/__init__.py
@@ -9,6 +9,10 @@ broadcast = Broadcast(settings.WS_MESSAGE_QUEUE)
 def create_app() -> FastAPI:
     app = FastAPI()
 
+    from project.logging import configure_logging
+
+    configure_logging()
+
     # do this before loading routes
     from project.celery_utils import create_celery
 
